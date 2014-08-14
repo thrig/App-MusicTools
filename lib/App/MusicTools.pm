9926cd7c7daa38cb1c42666b5a2c3c77491d5479
Jeremy Mates <jmates@cpan.org>
2014-08-14 04:05:46 +0000

    Author, author.

--- lib/App/MusicTools.pm
+++ lib/App/MusicTools.pm
@@ -104,7 +104,7 @@ L<Music::LilyPondUtil>, L<Music::Tension>
 
 =head1 AUTHOR
 
-Jeremy Mates, E<lt>jmates@cpan.orgE<gt>
+thrig - Jeremy Mates (cpan:JMATES) C<< <jmates at cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
